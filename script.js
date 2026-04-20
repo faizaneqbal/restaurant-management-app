996bc86debaeb43c43ee2dc995a7295f30906207
Faizan Eqbal <faizaneqbal1999@gmail.com>
2026-04-20 13:12:07 +0530

    Initial project setup with Bootstrap UI

---
+++ script.js
@@ -0,0 +1,16 @@
+function addOrder() {
+
+    const price = document.getElementById("price").value;
+    const dish = document.getElementById("dish").value;
+    const table = document.getElementById("table").value;
+
+    const li = document.createElement("li");
+    li.className = "list-group-item d-flex justify-content-between align-items-center";
+
+    li.innerHTML = `
+        <span><strong>${dish}</strong> - ₹${price}</span>
+        <button class="btn btn-sm btn-danger">Delete</button>
+    `;
+
+    document.getElementById(table).appendChild(li);
+}
